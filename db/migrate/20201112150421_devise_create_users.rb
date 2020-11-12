6901d7b744495c1d233245906bcf76154ee0d612
hiroki0456 <volleyball0456@gmail.com>
2020-11-13 00:13:27 +0900

    modified user migration and routing

--- db/migrate/20201112150421_devise_create_users.rb
+++ db/migrate/20201112150421_devise_create_users.rb
@@ -6,7 +6,9 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
       ## Database authenticatable
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
-
+      t.string :nickname,           null: false
+      t.integer :gender,            null: false
+      t.boolean :admin,             null: false, default: false
       ## Recoverable
       t.string   :reset_password_token
       t.datetime :reset_password_sent_at
