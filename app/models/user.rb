d90dca749a22a648d08b76f96a085dbb7e37a336
hiroki0456 <volleyball0456@gmail.com>
2020-11-13 19:51:07 +0900

    add validation user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,18 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  with_options presence: {message: "は必須です"} do
+    validates :nickname
+    validates :gender
+  end
+
+  validates :password, format: {with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{6,20}+\z/i, message: "半角英数字混合の6~20桁で入力してください"}
+
+  enum gender: {
+        female: 0,
+        male: 1,
+        other: 2,
+        not_apply: 4
+      }
 end
