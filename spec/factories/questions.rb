bf9f3bc07600beb23cce08a9942f92747266f4b5
hiroki0456 <volleyball0456@gmail.com>
2020-11-19 12:54:51 +0900

    modified question rspec

--- spec/factories/questions.rb
+++ spec/factories/questions.rb
@@ -1,9 +1,8 @@
 FactoryBot.define do
   factory :question do
-    category { 1 }
-    text { "MyString" }
-    title { "MyString" }
-    question { "MyText" }
-    user { nil }
+    category_id { 1 }
+    title { "a" * 20 }
+    question { "a" * 1000 }
+    association :user
   end
 end
