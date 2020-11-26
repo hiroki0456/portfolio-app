0d3bc5d72962490b0e4e5d3fd58969fd644ff527
hiroki0456 <volleyball0456@gmail.com>
2020-11-26 17:34:30 +0900

    question-index views

--- app/javascript/confirm.js
+++ app/javascript/confirm.js
@@ -1,5 +1,5 @@
 window.addEventListener('load', () => {
-    if (!document.querySelector('.btn')) return false
+    if (!document.querySelector('#btn')) return false
     document.querySelector('#question_question').addEventListener('input', function() {
         document.querySelector('.answer').innerHTML = this.value.length;
     });
