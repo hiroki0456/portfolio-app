13ffca1e6b5c62250be0d75814e55b56c41b4365
hiroki0456 <hiroki.mugikura@di-v.co.jp>
2021-03-21 10:28:43 +0900

    create spec-files

---
+++ spec/factories/philosopher_profiles.rb
@@ -0,0 +1,8 @@
+FactoryBot.define do
+  factory :philosopher_profile do
+    affiliation { "MyString" }
+    research { "MyText" }
+    profile_type { "MyString" }
+    profile_id { "" }
+  end
+end
