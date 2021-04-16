0180a441c5384eafa7c806f6f138cf80d765c220
hiroki0456 <hiroki.mugikura@di-v.co.jp>
2021-04-16 15:26:40 +0900

    environment variable

--- app/models/card.rb
+++ app/models/card.rb
@@ -2,7 +2,7 @@ class Card < ApplicationRecord
   belongs_to :user
 
   def self.card_create(card_token)
-    Payjp.api_key = "sk_test_d7339cf0cae7215730b6e296"
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
     customer = Payjp::Customer.create(
       card: card_token
     )
@@ -11,7 +11,7 @@ class Card < ApplicationRecord
 
   def self.pay(current_user)
     card = Card.where(user_id: current_user.id).first
-    Payjp.api_key = "sk_test_d7339cf0cae7215730b6e296"
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
     subscription = Payjp::Subscription.create(
       customer: card.customer_id,
       plan: Plan.first.plan_id,
@@ -21,14 +21,14 @@ class Card < ApplicationRecord
   end
 
   def self.show_card(card)
-    Payjp.api_key = "sk_test_d7339cf0cae7215730b6e296"
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
     customer = Payjp::Customer.retrieve(card.customer_id)
     card_information = customer.cards.retrieve(card.card_id)
     return card_information
   end
 
   def self.cancel(current_user)
-    Payjp.api_key = "sk_test_d7339cf0cae7215730b6e296"
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
     subscription = Payjp::Subscription.retrieve(current_user.subscription_id)
     customer = Payjp::Customer.retrieve(current_user.card.customer_id)
     subscription.cancel
