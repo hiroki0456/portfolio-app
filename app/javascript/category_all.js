0bacaef6becb2b9c51cf2249e61b4dcc48ed8a17
hiroki0456 <hiroki.mugikura@di-v.co.jp>
2021-05-05 12:14:29 +0900

    comment index

---
+++ app/javascript/category_all.js
@@ -0,0 +1,10 @@
+// window.addEventListener('load', function() {
+//     if (!document.querySelector('.nav-bar__category')) return false
+//     Array.from(document.querySelectorAll('.nav-bar__category')).forEach(function(category) {
+//         category.addEventListener('click', function() {
+//             let text = category.textContent;
+//             const XHR = new XMLHttpRequest();
+//             XHR.open('GET', ``)
+//         });
+//     })
+// });
