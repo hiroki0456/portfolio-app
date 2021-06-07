acef8c6b5e32c7efa8e910e4b10c2ffeed636292
hiroki0456 <hiroki.mugikura@di-v.co.jp>
2021-06-07 15:58:57 +0900

    modified capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,7 +5,7 @@ lock "~> 3.16.0"
 set :application, 'portfolio-app'
 
 # どのリポジトリからアプリをpullするかを指定する
-set :repo_url,  'git@github.com:hiroki0456/portfolio.git'
+set :repo_url,  'git@github.com:hiroki0456/portfolio-app.git'
 
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
