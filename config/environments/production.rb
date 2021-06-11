9b87d7145e144fee308c5d32f7db0c10874854ea
hiroki0456 <hiroki.mugikura@di-v.co.jp>
2021-06-11 20:31:06 +0900

    add s3

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -117,5 +117,5 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
-  Rails.application.routes.default_url_options[:host] = 'consultation-app-portfolio.herokuapp.com'
+  Rails.application.routes.default_url_options[:host] = 'http://52.192.219.78/'
 end
