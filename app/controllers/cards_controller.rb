6e551f2dfb3df151cdad2f962bec1bf765f26e2d
hiroki0456 <hiroki.mugikura@di-v.co.jp>
2021-06-12 21:05:22 +0900

    add transaction

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -8,15 +8,22 @@ class CardsController < ApplicationController
     if current_user.card
       redirect_to root_path
     else
-      customer = Card.card_create(params[:card][:token])
-      @card = Card.new(user_id: current_user.id, customer_id: customer.id, card_id: customer.default_card)
-      if @card.save
-        subscription = Card.pay(current_user)
-        current_user.update(subscription_id: subscription.id, premium: true)
-        flash[:alert] = '定期課金に登録できました'
+      begin
+        Card.transaction do
+          customer = Card.card_create(params[:card][:token])
+          @card = Card.new(user_id: current_user.id, customer_id: customer.id, card_id: customer.default_card)
+          if @card.save
+            subscription = Card.pay(current_user)
+            current_user.update(subscription_id: subscription.id, premium: true)
+            flash[:alert] = '定期課金に登録できました'
+            redirect_to root_path
+          else
+            render :new
+          end
+        end
+      rescue => e
+        flash[:alert] = '定期課金に失敗しました'
         redirect_to root_path
-      else
-        render :new
       end
     end
   end
